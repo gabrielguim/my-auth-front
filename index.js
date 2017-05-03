921fa3dfa44f82c07a4093c3b2f85cf38c7d4d8b
Gabriel Guimarães <gabrielguimoliver@gmail.com>
2017-05-03 20:56:46 -0300

    index.js node

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
  * Arquivo para criação de um servidor local
  * utilizado para executar o código de produção.
  *
- * @author Márcio Casale de Souza <contato@kazale.com>
- * @since 0.0.6
  */
 
 var path = require('path');
@@ -12,14 +10,14 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 // redireciona todas as requições para o Angular 2
 app.all('*', function(req, res) {
   res.status(200).sendFile(
-  	path.join(__dirname, 'dist', 'index.html'));
+  	path.join(__dirname, 'src', 'index.html'));
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node executando na porta ', app.get('port'));
+  console.log('Node server running on port: ', app.get('port'));
 });
